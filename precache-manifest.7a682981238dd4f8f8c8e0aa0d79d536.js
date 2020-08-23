4152fc7aab831e1da66ab3a5d5e75c893166ba3d
Arun <arunthulasiraman@gmail.com>
2020-08-23 23:24:34 +0530

    Initial dist subtree commit

---
+++ precache-manifest.7a682981238dd4f8f8c8e0aa0d79d536.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9b9cfbfd5dc7b5ad569f5c65fb5d4f9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a0eaa8fd83e2733f309b",
+    "url": "./static/css/main.11ea121e.chunk.css"
+  },
+  {
+    "revision": "b53d94e0602cb4d97ef0",
+    "url": "./static/js/2.8ca6bef5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.8ca6bef5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0eaa8fd83e2733f309b",
+    "url": "./static/js/main.b202334c.chunk.js"
+  },
+  {
+    "revision": "73032e6b2e3652cff02a",
+    "url": "./static/js/runtime-main.35cced3b.js"
+  },
+  {
+    "revision": "0baeaccdedd6fd0ec94283886a1e6334",
+    "url": "./static/media/testImage.0baeaccd.png"
+  }
+]);
